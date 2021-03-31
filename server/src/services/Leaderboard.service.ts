d1c1c500c7030e8bc0b0479fec3e9b9fee700808
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-03-31 10:47:36 +0200

    Add service for leaderboardController

---
+++ server/src/services/Leaderboard.service.ts
@@ -0,0 +1,37 @@
+import { AddResult, Score } from '../out';
+import ScoreModel from '../models/Score.model';
+import PlayerModel from '../models/Player.model';
+
+class LeaderboardService {
+
+  getTop10 = (): Promise<Score[]> => {
+
+    return ScoreModel.find().sort({score: 'desc'}).limit(10).populate("player")
+      .then((result: Score[]) => {
+        return result
+      })
+      .catch((e: { message: string | undefined; }) => {
+        new Error(e.message)
+      })
+  }
+
+  addResult = (resultFromReq: AddResult): Promise<Score> => {
+
+    let {score, player: name} = resultFromReq
+
+    const player = new PlayerModel({name})
+    player.save().then().catch((e: { message: string | undefined; }) => {
+      new Error(e.message)
+    })
+
+    return ScoreModel.create({
+      player,
+      score
+    }).then(result => result).catch((e: { message: string | undefined; }) => {
+      new Error(e.message)
+    })
+
+  }
+
+
+}
