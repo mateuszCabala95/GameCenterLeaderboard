bcec3ec441ed5f543407329c4588d60289b0e11a
Mateusz Cabała <mateusz.cabala95@gmail.com>
2021-03-31 14:01:49 +0200

    Add form for new result

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { LeaderboardsService } from '../out_spec';
+import { AddResult, LeaderboardsService } from '../out_spec';
 import { Score } from '../out_spec';
+import { FormControl, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  results$: Score[] = []
+  results$: Score[] = [];
   _service: LeaderboardsService;
 
-  constructor(private readonly service: LeaderboardsService ) {
-    this._service = service
+
+  addResultForm = new FormGroup({
+    player: new FormControl(''),
+    score: new FormControl()
+  });
+
+  constructor(private readonly service: LeaderboardsService) {
+    this._service = service;
   }
 
 
   ngOnInit() {
-    this.getTop10()
+    this.getTop10();
   }
 
-  getTop10(){
-    this._service.getTop10().subscribe(results => this.results$ = results)
+  getTop10() {
+    this._service.getTop10().subscribe(results => this.results$ = results);
   }
 
+  onSubmit(e) {
+    e.preventDefault();
+    this._service.addResult({
+      player: this.addResultForm.value.player,
+      score: this.addResultForm.value.score
+    }).subscribe(
+      () => {
+        this.getTop10();
+        this.addResultForm.reset()
+      }
+    );
+  }
 
 }
